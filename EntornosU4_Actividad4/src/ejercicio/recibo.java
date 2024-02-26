3e74d92720510debfe7c663a04211b56aa999a3f
YLJOSE <joserojasr47@gmail.com>
2024-02-26 19:47:11 +0100

    class recibo completo

--- EntornosU4_Actividad4/src/ejercicio/recibo.java
+++ EntornosU4_Actividad4/src/ejercicio/recibo.java
@@ -1,5 +1,47 @@
 package ejercicio;
-
+import java.util.Random;
 public class recibo {
+	private String nombre ;
+	private double cantidad;
+	
+	public recibo(String nombre, double cantidad) {
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		
+	}
+	
+	/**
+	 * @return el nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre a establecer
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return la cantidad
+	 */
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	/**
+	 * @param cantidad a establecer
+	 */
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
+	public boolean pagarRecibo () {
+		Random r = new Random();
+		double probabilidadPago = r.nextDouble(4) +1 ;
+		return probabilidadPago >1;
+	}
 
 }
